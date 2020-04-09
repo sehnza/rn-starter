468e1f765cf623b9d5839c3bc1020da0c7ea2f62
Hayden Steicke <hsteicke@gmail.com>
2020-04-09 13:53:20 +1000

    WindowsPC_Additons

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 
-const ImageDetail = () => {
-  return <Text>
-    Image Detail
-  </Text>
+const ImageDetail = ({ imageSource, title, score }) => {
+  return <>
+    <Image source={imageSource} />
+    <Text>{title}</Text>
+    <Text>Image Score - {score}</Text>
+  </>
 }
 
 const styles = StyleSheet.create({});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import ImageDetail from '../components/ImageDetail'
+import score from '../components/ImageDetail'
 
 const ImageScreen = () => {
   return <>
-    <ImageDetail />
-    <ImageDetail />
-    <ImageDetail />
-    <ImageDetail />
+    <ImageDetail title="Forest" Image Score='' imageSource={require('../../assets/forest.jpg')}
+      score={9}
+    />
+    <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')}
+      score={5}
+    />
+
+    <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')}
+      score={10}
+    />
+
   </>
 }
 
