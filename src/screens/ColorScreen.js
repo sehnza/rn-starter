53fe81d32960422a8362e1f3669674f9d06e6ee7
Hayden Steicke <hsteicke@gmail.com>
2020-04-10 10:00:02 +1000

    s6l47

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import { Button } from 'react-native'
+import React, {useState} from 'react'
+import { Button, View, FlatList } from 'react-native'
 import styled from 'styled-components'
 
 const ColorScreen = () => {
+    const [colors, setColors] = useState([]);
+    console.log(colors)
+
     return <>
-    <Button title='Add a Color' />
-    <View style={{ height: 100, width:100, backgroundColor: 'rgb(0, 255, 0)'}} />
-    </>
+    <Button title = 'Add a Color' onPress={() => {
+        setColors([...colors, randomRgb()])
+    }} />
+    
+    <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+            return <View style={{ height: 100, width: 500, backgroundColor: item }} />
+    }} />
+    </> 
+
+} 
+
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`
+
 }
 
-
 //styled components
-const Title = 
-styled.Text `font-size: ${props => props.large ? 45 : 20}px;`
+const Title = styled.Text`
+  font-size: ${props => props.large ? 45 : 20}px;
+`
 
 export default ColorScreen
